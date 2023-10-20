02804d2d2e66905d611f1398636f8310b765da3b
Colin N <colinn922@gmail.com>
2023-10-19 21:27:36 -0400

    Trying to make deployment work

--- TranquilTask/src/app/app.module.ts
+++ TranquilTask/src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core'; 
+import { NgModule, isDevMode } from '@angular/core'; 
 import { BrowserModule } 
 	from '@angular/platform-browser'; 
 import { AppRoutingModule } 
 	from './app-routing.module'; 
 import { AppComponent } from './app.component'; 
 import { HttpClientModule } 
-	from '@angular/common/http'; 
+	from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker'; 
 
 @NgModule({ 
 	declarations: [ 
@@ -14,7 +15,12 @@ import { HttpClientModule }
 	imports: [ 
 		BrowserModule, 
 		AppRoutingModule, 
-		HttpClientModule 
+		HttpClientModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: !isDevMode(),
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+}) 
 	], 
 	providers: [], 
 	bootstrap: [AppComponent] 
