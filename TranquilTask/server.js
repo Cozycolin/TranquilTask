2d90f26ca9a6d425b9242ab6a434d1de442b5711
Colin N <colinn922@gmail.com>
2023-10-21 13:22:23 -0400

    Mongo Atlas Connection Established

--- TranquilTask/server.js
+++ TranquilTask/server.js
@@ -1,14 +1,45 @@
 const express = require('express'); 
 const app = express(); 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 
 const port = 4000;
+
+
+// MongoDB client
+const uri = "mongodb+srv://TranquilTask:TranquilWebAdmin@tranquiltaskcluster.voe6es9.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+	serverApi: {
+	  version: ServerApiVersion.v1,
+	  strict: true,
+	  deprecationErrors: true,
+	}
+  });
+
+//MongoDB test
+async function run() {
+	try {
+		// Connect the client to the server	(optional starting in v4.7)
+		await client.connect();
+		// Send a ping to confirm a successful connection
+		await client.db("admin").command({ ping: 1 });
+		console.log("Pinged your deployment. You successfully connected to MongoDB!");
+	} finally {
+		// Ensures that the client will close when you finish/error
+		await client.close();
+	}
+}
+run().catch(console.dir);
+
 
 // handling CORS 
 var cors = require('cors');
 app.use(cors());
 
+
 //handle json
 app.use(express.json())
+
 
 // route for handling requests from the Angular client 
 app.get('/api/message', (req, res) => { 
